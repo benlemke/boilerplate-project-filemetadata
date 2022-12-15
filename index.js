688e3235d679026a67d8ec47be8041f295051523
benlemke <benlemke10@gmail.com>
2022-12-15 14:35:01 -0600

    completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var multer = require('multer');
+const upload = multer({ dest: 'public/'});
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,7 +13,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
